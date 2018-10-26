aebe16b473eae1d8252e5b8a9c94e759e43daf6a
Darruma <umerderr@gmail.com>
2018-10-26 21:37:44 +0100

    added poker table

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,31 +1,35 @@
 import React, { Component } from 'react'
 import '../css/poker.css'
-
+import '../css/cards.css'
 class Game extends Component {
     render() {
         return (
             <div className='poker-container'>
                 <div className='poker-table'>
+                    <div className='board-cards'>
 
+
+
+                    </div>
                 </div>
                 <div className='poker-ui'>
                     <div class='im'>
                         <div className='messages'>
 
-                        <p>Lol</p>
-                        <p>Lol</p>
-                        <p>Lol</p>
-                        
+                            <p>Lol</p>
+                            <p>Lol</p>
+                            <p>Lol</p>
+
 
                         </div>
                         <textarea className='input-message'></textarea>
                     </div>
 
-                <div className='play-buttons'>
-                    <button>Fold</button>
-                    <button>Check</button>
-                    <button>Raise</button>
-                </div>
+                    <div className='play-buttons'>
+                        <button className='poker-button'>Fold</button>
+                        <button className='poker-button'>Check</button>
+                        <button className='poker-button'>Raise</button>
+                    </div>
                 </div>
 
 
