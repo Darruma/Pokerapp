26ef65fd2dc9442f50de3569d4217d7e64c75c19
Darruma <umerderr@gmail.com>
2018-11-16 18:56:14 +0000

    changed profileReducer inital state

--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -1,8 +1,15 @@
 const initialState = {
     profileData:{},
     statistics:{},
-    myData:{} 
-    
+    myData:{
+        image:'',
+        posts:[],
+        priceData:[],
+        wins:0,
+        losses:0,
+        friends:[]
+    } 
+
 }
 const profileReducer = (state=initialState, action) => {
     switch (action.type) {
