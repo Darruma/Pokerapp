bcb70d59cda6b14f4cb01338ee308cdf401dc279
unknown <umerderr@gmail.com>
2019-02-06 12:42:45 +0000

    changed preinstall script

--- server.js
+++ server.js
@@ -8,14 +8,12 @@ const port = (process.env.PORT) ? process.env.PORT : 3005;
 server.listen(port, () => {
     console.log(`HTTPS Server running on port ${port}`);
 });
-const io = require('socket.io').listen(server)
 
 const accounts = require('./routes/accounts');
 const profiles = require('./routes/profiles');
 const tables = require('./routes/tables.js');
 
 const session = express_session({ secret: "oregano", resave: false, saveUninitialised: true });
-var sharedsession = require("express-socket.io-session");
 app.use(session);
 
 
@@ -30,9 +28,7 @@ app.use('/api', tables);
 
 
 
-// add messaging with socket.io
 app.use('/images', express.static(path.join(__dirname, '/client/images/')));
-app.use('/', express.static(path.join(__dirname, '/client/build')))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
 })
